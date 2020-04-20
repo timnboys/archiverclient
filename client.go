ac580b919f9b18e87e27bb467bdc97bf1aed59a6
Dot-Rar <dotrar@protonmail.com>
2020-04-20 17:56:10 +0100

    modmail support

--- client.go
+++ client.go
@@ -90,13 +90,71 @@ func (c *ArchiverClient) Store(messages []message.Message, guildId uint64, ticke
 	return nil
 }
 
-func (c *ArchiverClient) Encode(messages []message.Message, ticketId int) ([]byte, error) {
+func (c *ArchiverClient) GetModmail(guildId uint64, uuid string) ([]message.Message, error) {
+	endpoint := fmt.Sprintf("%s/modmail?guild=%d&uuid=%s", c.endpoint, guildId, uuid)
+	res, err := c.httpClient.Get(endpoint)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		if res.StatusCode == 404 {
+			return nil, ErrExpired
+		}
+
+		var decoded map[string]string
+		if err := json.NewDecoder(res.Body).Decode(&decoded); err != nil {
+			return nil, err
+		}
+
+		return nil, errors.New(decoded["message"])
+	} else {
+		var messages []message.Message
+		if err := json.NewDecoder(res.Body).Decode(&messages); err != nil {
+			return nil, err
+		}
+
+		return messages, nil
+	}
+}
+
+func (c *ArchiverClient) StoreModmail(messages []message.Message, guildId uint64, uuid string, premium bool) error {
+	encoded, err := json.Marshal(messages)
+	if err != nil {
+		return err
+	}
+
+	endpoint := fmt.Sprintf("%s/modmail?guild=%d&id=%s", c.endpoint, guildId, uuid)
+	if premium {
+		endpoint += "&premium"
+	}
+
+	res, err := c.httpClient.Post(endpoint, "application/json", bytes.NewReader(encoded))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		var decoded map[string]string
+		if err := json.NewDecoder(res.Body).Decode(&decoded); err != nil {
+			return err
+		}
+
+		return errors.New(decoded["message"])
+	}
+
+	return nil
+}
+
+func (c *ArchiverClient) Encode(messages []message.Message, title string) ([]byte, error) {
 	encoded, err := json.Marshal(messages)
 	if err != nil {
 		return nil, err
 	}
 
-	endpoint := fmt.Sprintf("%s/encode?id=%d", c.endpoint, ticketId)
+	endpoint := fmt.Sprintf("%s/encode?title=%s", c.endpoint, title)
 	res, err := c.httpClient.Post(endpoint, "application/json", bytes.NewReader(encoded))
 	if err != nil {
 		return nil, err
